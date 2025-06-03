f1e8bb9d0c1886e5ccda2b21a501971251551ddd
Alistair Turnbull <github@minworks.co.uk>
2025-06-03 17:45:56 +0100

    Add `intro.html`, including a questionnaire.

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ use std::collections::{HashMap};
 use std::error::{Error};
 use std::io::{Write};
 use std::fs::{File};
-use std::str::{Split};
 
 use tiny_http::{Method, Request, Response, Header};
 use url::{Url};
@@ -50,12 +49,16 @@ impl_from_for_error!(url::ParseError);
 impl_from_for_error!(png::EncodingError);
 impl_from_for_error!(png::DecodingError);
 
-fn header(key: &str, value: &str) -> tiny_http::Header {
-    let key_b = key.as_bytes();
-    let val_b = value.as_bytes();
-    Header::from_bytes(
-        key_b, val_b)
-        .unwrap() // depends only on data fixed at compile time
+// ----------------------------------------------------------------------------
+
+/// Represent HTTP request parameters.
+#[derive(Debug)]
+pub struct Params(HashMap<String, String>);
+
+impl Params {
+    fn get(&self, key: &str) -> Result<&String, HttpError> {
+        self.0.get(key).ok_or(HttpError::Invalid)
+    }
 }
 
 // ----------------------------------------------------------------------------
@@ -168,6 +171,15 @@ impl Ocularity {
         server
     }
 
+    /// Construct an HTTP header.
+    fn header(key: &str, value: &str) -> tiny_http::Header {
+        let key_b = key.as_bytes();
+        let val_b = value.as_bytes();
+        Header::from_bytes(
+            key_b, val_b)
+            .unwrap() // depends only on data fixed at compile time
+    }
+
     /// Handle requests for ever.
     fn handle_requests(&self) {
         for request in self.server.incoming_requests() {
@@ -179,19 +191,19 @@ impl Ocularity {
                     request.respond(Response::from_string(text))
                 },
                 Ok(HttpOkay::Html(text)) => {
-                    let header = header("Content-Type", "text/html");
+                    let header = Self::header("Content-Type", "text/html");
                     request.respond(Response::from_string(text).with_header(header))
                 },
                 Ok(HttpOkay::Data(data)) => {
-                    let header = header("Content-Type", "image/png");
+                    let header = Self::header("Content-Type", "image/png");
                     request.respond(Response::from_data(data).with_header(header))
                 },
                 Ok(HttpOkay::Static(data, content_type)) => {
-                    let header = header("Content-Type", content_type);
+                    let header = Self::header("Content-Type", content_type);
                     request.respond(Response::from_data(data).with_header(header))
                 },
                 Ok(HttpOkay::Redirect(relative_url)) => {
-                    let header = header("Location", self.base_url.join(&relative_url).unwrap().as_str());
+                    let header = Self::header("Location", self.base_url.join(&relative_url).unwrap().as_str());
                     request.respond(Response::from_string("Moved Permanently").with_status_code(301).with_header(header))
                 },
                 Err(HttpError::Invalid) => {
@@ -208,6 +220,9 @@ impl Ocularity {
         }
     }
 
+    const STYLESHEET: &[u8] = include_bytes!("stylesheet.css");
+    const INTRO: &[u8] = include_bytes!("intro.html");
+
     /// Handle a single request.
     fn handle_request(&self, request: &Request) -> Result<HttpOkay, HttpError> {
         match request.method() {
@@ -219,27 +234,19 @@ impl Ocularity {
         let url = url_escape::decode(url).into_owned();
         let url = self.base_url.join(&url)?;
         println!("{} {}", request.remote_addr().unwrap().ip(), url);
-        let params: HashMap<String, String> = url.query_pairs().map(
+        let params = Params(url.query_pairs().map(
             |(key, value)| (key.into_owned(), value.into_owned())
-        ).collect();
+        ).collect());
         let mut path = url.path_segments().unwrap();
         match path.next() {
-            Some("static") => Self::static_file(path, params),
-            Some("image.png") => Self::image(path, params),
-            Some("question") => Self::question(path, params),
-            Some("submit") => self.submit(request.remote_addr().unwrap(), params),
-            _ => Err(HttpError::NotFound),
-        }
-    }
-
-
-    const STYLESHEET: &[u8] = include_bytes!("stylesheet.css");
-
-    /// Serve a static file.
-    fn static_file(mut path: Split<char>, _params: HashMap<String, String>) -> Result<HttpOkay, HttpError> {
-        match path.next() {
+            None | Some("") | Some("index.html") => Ok(HttpOkay::Redirect("intro.html".into())),
             Some("stylesheet.css") => Ok(HttpOkay::Static(Self::STYLESHEET, "text/css")),
-            _ => Err(HttpError::Invalid),
+            Some("intro.html") => Ok(HttpOkay::Static(Self::INTRO, "text/html")),
+            Some("image.png") => Self::image(params),
+            Some("question") => Self::question(params),
+            Some("start") => self.start(params),
+            Some("submit") => self.submit(request.remote_addr().unwrap(), params),
+            p => { println!("Not found: {:?}", p); Err(HttpError::NotFound) },
         }
     }
 
@@ -257,9 +264,9 @@ impl Ocularity {
     const TEST_PATTERN: &[u8] = include_bytes!("test-pattern-grey.png");
 
     /// Serve an image file.
-    pub fn image(_path: Split<char>, params: HashMap<String, String>) -> Result<HttpOkay, HttpError> {
-        let bg = Self::parse_colour(params.get("bg").ok_or(HttpError::Invalid)?)?;
-        let fg = Self::parse_colour(params.get("fg").ok_or(HttpError::Invalid)?)?;
+    pub fn image(params: Params) -> Result<HttpOkay, HttpError> {
+        let bg = Self::parse_colour(params.get("bg")?)?;
+        let fg = Self::parse_colour(params.get("fg")?)?;
 
         // Construct the palette.
         let mut palette = Vec::new();
@@ -304,18 +311,20 @@ impl Ocularity {
     /// - win2 - the foreground colour for this image.
     /// - lose1 - the background colour for the other image.
     /// - lose2 - the foreground colour for the other image.
-    fn form_element(which: usize, win: (Colour, Colour), lose: (Colour, Colour)) -> String {
+    fn form_element(questionnaire: &str, which: usize, win: (Colour, Colour), lose: (Colour, Colour)) -> String {
         format!(
             r#"
-                                    <form action="/submit">
-                                        <input type="hidden" name="which" value="{}">
-                                        <input type="hidden" name="win1" value="{}"/>
-                                        <input type="hidden" name="win2" value="{}"/>
-                                        <input type="hidden" name="lose1" value="{}"/>
-                                        <input type="hidden" name="lose2" value="{}"/>
-                                        <input type="image" src="/image.png?bg={}&fg={}"/>
-                                    </form>
+                <form action="/submit">
+                    <input type="hidden" name="q" value="{}">
+                    <input type="hidden" name="which" value="{}">
+                    <input type="hidden" name="win1" value="{}"/>
+                    <input type="hidden" name="win2" value="{}"/>
+                    <input type="hidden" name="lose1" value="{}"/>
+                    <input type="hidden" name="lose2" value="{}"/>
+                    <input type="image" src="/image.png?bg={}&fg={}"/>
+                </form>
             "#,
+            questionnaire,
             which,
             win.0, win.1,
             lose.0, lose.1,
@@ -324,7 +333,8 @@ impl Ocularity {
     }
 
     /// Returns a question comparing two images.
-    pub fn question(_path: Split<char>, _params: HashMap<String, String>) -> Result<HttpOkay, HttpError> {
+    pub fn question(params: Params) -> Result<HttpOkay, HttpError> {
+        let questionnaire = params.get("q")?;
         let pair1 = Self::random_colour_pair();
         let pair2 = Self::random_colour_pair();
         Ok(HttpOkay::Html(format!(
@@ -333,9 +343,9 @@ impl Ocularity {
                 <html>
                     <head>
                         <title>Click on the one that is most visible</title>
-                        <link rel="stylesheet" href="/static/stylesheet.css">
+                        <link rel="stylesheet" href="/stylesheet.css">
                     </head>
-                    <body>
+                    <body class="grey">
                         <div class="box">
                             <p class="instruction">Click on the image where the text is most easily visible.</p>
                             <div class="question">
@@ -348,31 +358,49 @@ impl Ocularity {
                     </body>
                 </html>
             "#,
-            Self::form_element(1, pair1, pair2),
-            Self::form_element(2, pair2, pair1),
+            Self::form_element(questionnaire, 1, pair1, pair2),
+            Self::form_element(questionnaire, 2, pair2, pair1),
         )))
     }
 
+    /// Start the experiment.
+    pub fn start(&self, params: Params) -> Result<HttpOkay, HttpError> {
+        let questionnaire = format!(
+            "{}{}{}{}{}{}{}{}{}{}{}{}",
+            params.get("age")?,
+            params.get("sex")?,
+            params.get("eye_colour")?,
+            params.get("cvd")?,
+            params.get("eyewear")?,
+            params.get("surgery")?,
+            params.get("where")?,
+            params.get("light")?,
+            params.get("company")?,
+            params.get("device")?,
+            params.get("screen")?,
+            params.get("monochrome")?,
+        );
+        Ok(HttpOkay::Redirect(format!("/question?q={}", questionnaire)))
+    }
+
     /// Log the answer to a `question()`.
-    pub fn submit(
-        &self,
-        remote_addr: &std::net::SocketAddr,
-        params: HashMap<String, String>,
-    ) -> Result<HttpOkay, HttpError> {
-        let which = params.get("which").ok_or(HttpError::Invalid)?.parse::<u8>()?;
+    pub fn submit(&self, remote_addr: &std::net::SocketAddr, params: Params) -> Result<HttpOkay, HttpError> {
+        let questionnaire = params.get("q")?;
+        let which = params.get("which")?.parse::<u8>()?;
         let is_first = which == 1;
-        let win1 = Self::parse_colour(params.get("win1").ok_or(HttpError::Invalid)?)?;
-        let win2 = Self::parse_colour(params.get("win2").ok_or(HttpError::Invalid)?)?;
-        let lose1 = Self::parse_colour(params.get("lose1").ok_or(HttpError::Invalid)?)?;
-        let lose2 = Self::parse_colour(params.get("lose2").ok_or(HttpError::Invalid)?)?;
-        writeln!(&self.results, "{}, {}, {}, {}, {}, {}, {}",
+        let win1 = Self::parse_colour(params.get("win1")?)?;
+        let win2 = Self::parse_colour(params.get("win2")?)?;
+        let lose1 = Self::parse_colour(params.get("lose1")?)?;
+        let lose2 = Self::parse_colour(params.get("lose2")?)?;
+        writeln!(&self.results, "{}, {}, {}, {}, {}, {}, {}, {}",
             remote_addr.ip(),
             chrono::Utc::now(),
+            questionnaire,
             is_first,
             win1, win2,
             lose1, lose2,
         )?;
-        Ok(HttpOkay::Redirect("question".into()))
+        Ok(HttpOkay::Redirect(format!("/question?q={}", questionnaire)))
     }
 }
 
